4a6a4b35b37ef943e7e1ce1deff27e923ade8400
Thomaslam2142 <thomaslam2142@gmail.com>
2023-09-10 20:34:22 -0400

    ok im confused. Server is connected I think

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
-const port = 3000; // You can choose any port you like
+const port = process.env.PORT || 3000; // Use process.env.PORT for Heroku deployment, default to 3000
 
 // Define a route
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-// Start the server
+const mongoose = require('mongoose');
+
+// Replace 'your_connection_string' with your actual MongoDB connection string
+const mongoURI = 'mongodb://localhost:27017/myappdb';
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Start the Express server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
